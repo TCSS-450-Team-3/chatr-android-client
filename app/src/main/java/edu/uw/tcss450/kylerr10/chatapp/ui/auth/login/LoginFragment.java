562e603fe2d9bf90d8b96a81228685f569e9f83b
Bettyabera <bettyabe@uw.edu>
2023-05-18 16:32:08 -0700

    Email

--- app/src/main/java/edu/uw/tcss450/kylerr10/chatapp/ui/auth/login/LoginFragment.java
+++ app/src/main/java/edu/uw/tcss450/kylerr10/chatapp/ui/auth/login/LoginFragment.java
@@ -1,9 +1,11 @@
 package edu.uw.tcss450.kylerr10.chatapp.ui.auth.login;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,18 +13,31 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import edu.uw.tcss450.kylerr10.chatapp.R;
 import edu.uw.tcss450.kylerr10.chatapp.databinding.FragmentLoginBinding;
+import edu.uw.tcss450.kylerr10.chatapp.ui.auth.register.VerifyEmailFragment;
 
 /**
  * A simple {@link Fragment} subclass responsible for validating user credentials.
- * @author Jasper Newkirk
+ * @author Jasper Newkirk,Betelhem
  */
 public class LoginFragment extends Fragment {
 
     public FragmentLoginBinding mBinding;
+    private boolean buttonClicked = false;
 
     private LoginViewModel mViewModel;
+
+    private String userEmail;
 
     public static LoginFragment newInstance() {
         return new LoginFragment();
@@ -42,16 +57,64 @@ public class LoginFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_login, container, false);
     }
 
+
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         mBinding = FragmentLoginBinding.bind(requireView());
-
+        String url = "http://10.0.2.2:5000/verify/send-email";
         mBinding.buttonLogin.setOnClickListener(button -> {
-            Navigation.findNavController(requireView()).navigate(
-                    R.id.action_loginFragment_to_homeActivity
+            // Check if the button is already clicked
+            if (buttonClicked) {
+                return; // Ignore subsequent clicks
+            }
+
+            // Set the button as clicked
+            buttonClicked = true;
+
+            // Get the email entered by the user
+            String email = mBinding.editEmail.getText().toString();
+            userEmail = email;
+            Log.d("EMAIL", email);
+
+            VerifyEmailFragment verifyEmailFragment = new VerifyEmailFragment();
+            Bundle bundle = new Bundle();
+            bundle.putString("email", email);
+            verifyEmailFragment.setArguments(bundle);
+            // Create the request body
+            JSONObject requestBody = new JSONObject();
+            try {
+                requestBody.put("email", email);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            // Make a request to send the verification email
+            JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, url, requestBody,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            // Email sent successfully
+                            Toast.makeText(requireContext(), "Email sent successfully", Toast.LENGTH_SHORT).show();
+                            Log.d("Response", response.toString());
+                            // Proceed to the verification fragment
+                            Navigation.findNavController(requireView()).navigate(R.id.action_loginFragment_to_verifyEmailFragment);
+
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Toast.makeText(requireContext(), "Failed to send email", Toast.LENGTH_SHORT).show();
+                            Log.e("Volley", error.toString());
+                            buttonClicked = false;
+                        }
+                    }
             );
-            getActivity().finish();
+
+            // Add the request to the request queue
+            Volley.newRequestQueue(requireContext()).add(request);
         });
 
         mBinding.buttonToRegister.setOnClickListener(button -> {
@@ -64,5 +127,6 @@ public class LoginFragment extends Fragment {
         mBinding.editPassword.setText(args.getPassword().equals("default") ? "" : args.getPassword());
 
     }
+
 
 }
--- app/src/main/java/edu/uw/tcss450/kylerr10/chatapp/ui/auth/register/VerifyEmailFragment.java
+++ app/src/main/java/edu/uw/tcss450/kylerr10/chatapp/ui/auth/register/VerifyEmailFragment.java
@@ -1,15 +1,28 @@
 package edu.uw.tcss450.kylerr10.chatapp.ui.auth.register;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import edu.uw.tcss450.kylerr10.chatapp.R;
 import edu.uw.tcss450.kylerr10.chatapp.databinding.FragmentVerifyEmailBinding;
@@ -17,11 +30,12 @@ import edu.uw.tcss450.kylerr10.chatapp.databinding.FragmentVerifyEmailBinding;
 /**
  * Fragment for user email verification.
  *
- * @author Kyler Robison
+ * @author Kyler Robison, Betelhem
  */
 public class VerifyEmailFragment extends Fragment {
 
     private FragmentVerifyEmailBinding mBinding;
+    private String email;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -37,11 +51,68 @@ public class VerifyEmailFragment extends Fragment {
         return mBinding.getRoot();
     }
 
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        Log.d("VerificationFragment", "onViewCreated called");
         super.onViewCreated(view, savedInstanceState);
+        String url = "http://10.0.2.2:5000/verify/verify-otp";
 
-        mBinding.buttonVerify.setOnClickListener(button -> Navigation.findNavController(
-                        getView()).navigate(R.id.action_verifyEmailFragment_to_homeActivity));
+        Bundle bundle = getArguments();
+        if (bundle != null && bundle.containsKey("email")) {
+            String email = bundle.getString("email");
+            setEmail(email);
+        }
+        mBinding.buttonVerify.setOnClickListener(button -> {
+            String otpCode = mBinding.editCode.getText().toString();
+            Log.d("VerificationFragment", "OTP Code: " + otpCode);
+            Log.d("VerificationFragment", "Email: " + email);
+            // Create the request body
+            JSONObject requestBody = new JSONObject();
+            try {
+                requestBody.put("email", email);
+                requestBody.put("otp", otpCode);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            Log.e("Request Body", requestBody.toString());
+            // Make a request to verify the OTP code
+            JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, requestBody,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            // OTP code verified successfully
+                            Toast.makeText(requireContext(), "OTP verified successfully", Toast.LENGTH_SHORT).show();
+                            Log.d("Response", response.toString());
+                            // Proceed to the home fragment
+                            Navigation.findNavController(requireView()).navigate(R.id.action_verifyEmailFragment_to_homeActivity);
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Toast.makeText(requireContext(), "Failed to verify OTP", Toast.LENGTH_SHORT).show();
+                            Log.e("Volley", error.toString());
+                        }
+                    }
+            ){
+                @Override
+                public Map<String, String> getHeaders() {
+                    Map<String, String> headers = new HashMap<>();
+                    // Add authentication headers
+                    headers.put("Authorization", "Bearer Chat&%$key8329dogsrule");
+                    return headers;
+                }
+            };
+
+            // Add the request to the request queue
+            Volley.newRequestQueue(requireContext()).add(request);
+        });
+    }
+
+    public void setEmail(String email) {
+        Log.d("VerificationFragment", "setEmail invoked with email: " + email);
+        this.email = email;
     }
 }
+
